4fd67bf1b014bc0b0eb47361357100cdd44490b5
RomanZorkin <zorkin.roman@gmail.com>
2022-05-29 08:32:38 +0700

    feature: drop sceleton

--- tester/make_csv.py
+++ tester/make_csv.py
@@ -7,13 +7,14 @@ from tester import config
 logger = logging.getLogger(__name__)
 
 
-def write_csv(boxes: list[str], file_name: str):
+def write_csv(boxes: list[list[int]], file_name: str):
     csv_file = Path(config.CSV_DIR, f'{file_name}.csv')
 
     with open(csv_file, 'w', newline='') as myfile:
-        wr = csv.writer(myfile, delimiter='\n', quoting=csv.QUOTE_ALL)
-        wr.writerow([f'x,y'])
-        wr.writerow(boxes)
+        wr = csv.writer(myfile, delimiter=';', quoting=csv.QUOTE_ALL)
+        wr.writerow(['x', 'y'])
+        for row in boxes:
+            wr.writerow(row)
 
 
 def create_csv(idxs, boxes, file_name: str):
@@ -27,6 +28,6 @@ def create_csv(idxs, boxes, file_name: str):
         x_center = (x_1 + x_2) // 2
         y_center = (y_1 + y_2) // 2
 
-        centers.append(f'{x_center},{y_center}')
+        centers.append([x_center, y_center])
 
     write_csv(centers, file_name)
